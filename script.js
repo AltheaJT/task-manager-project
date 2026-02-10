8249575d75feda72ceac1185d0efc54226023aa0
AltheaJT <jindumorsindi@gmail.com>
2026-02-10 11:52:58 +0100

    update

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const addTaskBtn = document.getElementById("add-task-button");
+const closeBtn = document.getElementById("close-btn");
 const taskList = document.getElementById("task-items-container"); //list-container
-const filterBtn = document.getElementById("filter-button");
+const filterList= document.getElementById("task-filter-options");
 let allTasks = [];
 let lastTaskId = 0;
+
 
 class TodoTask {
     constructor(id,title,priority,completed=false,dueDate) {
@@ -15,67 +17,94 @@ class TodoTask {
     }
 }
 
-addTaskBtn.addEventListener('click', function() {
-    openModal();
+addTaskBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", () => {
+    overlay.classList.remove('active');
 });
-/*filterBtn.addEventListener('click', function(event) {
-   event.stopPropagation;
-    toggleFilterDropdown();
-} )*/
 
 function createUniqueId(){
-    let timeNow = Date.now();
+    let taskId = Date.now();
 
-    if (timeNow <= lastTaskId) {
+    if (taskId <= lastTaskId) {
         lastTaskId++;
         return 'task-' + lastTaskId;
     }
 
-    lastTaskId = timeNow;
-    return 'task-' + timeNow;
+    lastTaskId = taskId;
+    return 'task-' + taskId;
 }
 
 let isEditingMode = false;
-let title= document.getElementById("modal-title");
-let taskTitle = document.getElementById("task-title");
-let priorityInput = document.getElementById("task-priority");
-let dueDateInput = document.getElementById("task-due-date");
+const title= document.getElementById("modal-title");
+const taskTitle = document.getElementById("task-title");
+const priorityInput = document.getElementById("task-priority");
+const dueDateInput = document.getElementById("task-due-date");
+const overlay = document.getElementById("modal-overlay");
 
 //Modal Functions
 //Modal for adding tasks (not displaying, however)
 function openModal(){
     isEditingMode = false;
 
+    console.log('Modal is opened');
     title.textContent = 'Add New Task';
     taskTitle.value = '';
     taskTitle.placeholder = 'New Task!';
-    priorityInput.value = '';
+    priorityInput.value = 'low'; //Default priority
     dueDateInput.value = '';
 
-    let overlay = document.getElementById('modal-overlay');
     overlay.classList.add('active');
-    taskTitle.focus();
 }
 
 function openEditModal(taskId){
-    isEditingMode = true;
-}
+    isEditingMode = true; 
 
-function addTask() {
-    let newTask = new TodoTask(createUniqueId(), taskTitle.value, priorityInput.value, false, dueDateInput.value);
-    allTasks.push(newTask);
     
-    let overlay = document.getElementById('modal-overlay');
-    overlay.classList.remove('active');
-
-    renderTasks();
+    title.textContent = 'Edit Task';
+    taskTitle.value = taskId.title;
+    priorityInput.value = taskId.priority;
+    dueDateInput.value =  taskId.dueDate;
 }
 
-function editTask(){
 
-}
+class TaskManager {
+    constructor() {
+        this.tasks = [];
 
-function deleteTask(){
+    }
+
+    addTask() {
+        let newTask = new TodoTask(createUniqueId(), taskTitle.value, priorityInput.value, false, dueDateInput.value);
+        this.tasks.push(newTask);
+        
+        overlay.classList.remove('active');
+        renderTasks();
+    }
+
+    editTask(taskId, newTitle, newPriority, newDueDate) {
+        for(let i = 0; i < this.tasks.length; i++) {
+            if(this.tasks[i].id === taskId) {
+                this.tasks[i].title = newTitle;
+                this.tasks[i].priority = newPriority;
+                this.tasks[i].dueDate = newDueDate;
+                break;
+            }
+        }
+    }
+
+    deleteTask(taskId) {
+        let newTaskList = [];
+        for(let i = 0; i < this.tasks.length; i++) {
+            if(this.tasks[i].id !== taskId) {
+                newTaskList.push(this.tasks[i]);
+            }
+        }
+        this.tasks = newTaskList;  
+    }
+
+    toggleTaskCompletion(taskId) {
+        
+    }
 
 }
 
